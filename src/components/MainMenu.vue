2928ed0932dcd6e2ae8d1776070daabce91f6e72
Gomulko <rafa.gomu1994@gmail.com>
2022-06-22 22:46:17 +0200

    init commit

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in menuItems" :key="item.id">
+        <menu-item :name="item.name"/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import MenuItem from "./MenuItem.vue";
+
+export default {
+  components: { MenuItem },
+  data() {
+    return {
+      menuItems: [
+        { name: "Zgłoś", id: 1 },
+        { name: "Nasza Misja", id: 2 },
+        { name: "Kontakt", id: 3 },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <router-link :to='name'>{{name}}</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['name']
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
